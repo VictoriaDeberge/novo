982e67237a76c2c53afda2cdbbec28291f3b12cf
Victoria Deberge Beauty Salon <112016091+VictoriaDeberge@users.noreply.github.com>
2022-12-01 17:00:38 -0300

    Add files via upload

---
+++ Victoria Deberge/index.php
@@ -0,0 +1,24 @@
+<?php 
+ require_once 'Menu.php';
+?>
+
+<div class="row">
+    <div class="cos-md-12">
+        <img
+
+
+
+
+
+
+
+<?php 
+ require_once 'footer.php';
+ <div class "row">
+ <img src:"1.jpg">
+ <img src:"capa.jpg"
+?</div>
+
+
+
+ 
